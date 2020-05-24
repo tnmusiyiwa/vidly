98e8cec52d17b35666b8b66d7a99e2638aa48480
Tafadzwa Musiyiwa <tnmusiyiwa@gmail.com>
2020-05-24 16:35:28 +0200

    sorting

---
+++ src/components/movies.jsx
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService';
+import { getGenres } from '../services/fakeGenreService';
+import ListGroup from './common/listGroup';
+import Pagination from './common/pagination';
+import { paginate } from '../utils/paginate';
+import MoviesTable from './moviesTable';
+import _ from 'lodash';
+
+class Movies extends Component {
+  state = {
+    movies: [],
+    genres: [],
+    pageSize: 4,
+    currentPage: 1,
+    sortColumn: { path: 'title', order: 'asc' },
+  };
+
+  componentDidMount() {
+    const genres = [{ _id: '', name: 'All Genres' }, ...getGenres()];
+
+    this.setState({ movies: getMovies(), genres });
+  }
+
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+
+    this.setState({ movies });
+  };
+
+  handleGenreSelect = (genre) => {
+    this.setState({ selectedGenre: genre, currentPage: 1 });
+  };
+
+  handleSort = (sortColumn) => {
+    this.setState({ sortColumn });
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+    const {
+      currentPage,
+      pageSize,
+      movies: allMovies,
+      sortColumn,
+      selectedGenre,
+    } = this.state;
+
+    if (count === 0) return <p>There are no movies in the database.</p>;
+
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+        : allMovies;
+
+    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+    const movies = paginate(sorted, currentPage, pageSize);
+
+    return (
+      <div className='row'>
+        <div className='col-3'>
+          <ListGroup
+            items={this.state.genres}
+            selectedItem={this.state.selectedGenre}
+            onItemSelect={this.handleGenreSelect}
+          />
+        </div>
+        <div className='col'>
+          <p>Showing {filtered.length} movies in the database.</p>
+          <MoviesTable
+            movies={movies}
+            sortColumn={sortColumn}
+            onDelete={this.handleDelete}
+            onLike={this.handleLike}
+            onSort={this.handleSort}
+          />
+          <Pagination
+            itemsCount={filtered.length}
+            pageSize={this.state.pageSize}
+            currentPage={this.state.currentPage}
+            onPageChange={this.handlePageChange}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Movies;
---
+++ src/components/moviesTable.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import Like from './common/like';
+
+class MoviesTable extends Component {
+  raiseSort = (path) => {
+    const sortColumn = { ...this.props.sortColumn };
+    if (sortColumn.path === path) {
+      sortColumn.order = sortColumn.order === 'asc' ? 'desc' : 'asc';
+    } else {
+      sortColumn.path = path;
+      sortColumn.order = 'asc';
+    }
+
+    this.props.onSort(sortColumn);
+  };
+
+  render() {
+    const { movies, onDelete, onLike } = this.props;
+
+    return (
+      <table className='table'>
+        <thead>
+          <tr>
+            <th onClick={() => this.raiseSort('title')} scope='col'>
+              Title
+            </th>
+            <th onClick={() => this.raiseSort('genre.name')} scope='col'>
+              Genre
+            </th>
+            <th onClick={() => this.raiseSort('numberInStock')} scope='col'>
+              Stock
+            </th>
+            <th onClick={() => this.raiseSort('dailyRentalRate')} scope='col'>
+              Rate
+            </th>
+            <th scope='col'></th>
+            <th scope='col'></th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => {
+            return (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like liked={movie.liked} onClick={() => onLike(movie)} />
+                </td>
+                <td>
+                  <button
+                    onClick={() => onDelete(movie)}
+                    className='btn btn-danger btn-sm'
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default MoviesTable;
